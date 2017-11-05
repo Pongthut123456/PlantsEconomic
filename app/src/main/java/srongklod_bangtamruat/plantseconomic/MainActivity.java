e4c1e546df46f394a40e08ab6556ed291a3eda56
aod <1234aodto@gmail.com>
2017-11-05 12:00:53 +0700

    Main Ok

--- app/src/main/java/srongklod_bangtamruat/plantseconomic/MainActivity.java
+++ app/src/main/java/srongklod_bangtamruat/plantseconomic/MainActivity.java
@@ -3,6 +3,8 @@ package srongklod_bangtamruat.plantseconomic;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import srongklod_bangtamruat.plantseconomic.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,7 +12,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//        Add fRagment to Activity
+        if (savedInstanceState == null) {
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragment()).commit();
+
+        }
 
 
-    }
+    } // Main Method
+
 }//Main Class
